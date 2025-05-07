782a4176100b67d95a2d30a3fd55365b690a9950
Niek Fraaije <niekfraaije@gmail.com>
2025-05-07 12:22:25 +0200

    go_pipeline.py Both methods have their own prepare_run, in method 1 (the new one) the unique molecules in the system are identified, so that all other molecules that are the same, have the same mol.meta[‘moltype’] (= f"{moltype}_{reference_chain_id}"). As you can see the meta[‘moltype’] of each homologous molecule is set to the reference_chain_id, which is the name of the first chain in this set of homologous molecules. This is important because then only 1 itp file is written for each unique molecule.
    There are some dictionaries made for mapping in the 2 systems that are used (merged and non-merged). these mappings are put into system.go_params
    The first is chain_to_mol_id, which gives a mapping of the chain (‘A’, ‘B’, ‘C’, etc.) to the mol id in the system (0, 1, 2, etc.)
    The second is unique_mols_map, which gives a mapping for each chain, to all the chains that are homologous/the same (e. g. {'A': ['B', 'C', 'D'], 'B': ['A', 'C', 'D'], 'C': ['A', 'B', 'D'], 'D': ['A', 'B', 'C'], 'E': []})
    The third is reference_chain_map, which maps the chain to the ‘mother chain’, which is the reference_chain_id that was mentioned earlier. (e.g. {'A': 'A', 'B': 'A', 'C': 'A', 'D': 'A', 'E': 'E'})
    
    Then as usual the system is put through the 3 processes. SetMoleculeMeta, VirtualSiteCreator and ComputeStructuralGoBias, which point to the set_molecule_meta.py, go_vs_includes.py and go_structure_bias.py scripts.
    in the set_molecule_meta.py nothing was changed, it just correctly sets the meta of each molecule in the system.

--- vermouth/rcsu/go_pipeline.py
+++ vermouth/rcsu/go_pipeline.py
@@ -21,6 +21,7 @@ from ..processors.processor import Processor
 from .go_vs_includes import VirtualSiteCreator
 from .go_structure_bias import ComputeStructuralGoBias
 from ..processors import SetMoleculeMeta
+from collections import defaultdict
 
 class GoProcessorPipeline(Processor):
     """
@@ -30,7 +31,7 @@ class GoProcessorPipeline(Processor):
         self.processor_list = processor_list
         self.kwargs = {}
 
-    def prepare_run(self, system, moltype):
+    def prepare_run_method_0(self, system, moltype):
         """
         Things to do before running the pipeline.
         """
@@ -41,13 +42,44 @@ class GoProcessorPipeline(Processor):
         molecule = system.molecules[0]
         molecule.meta['moltype'] = moltype
 
+    def prepare_run_method_1(self, system, moltype):
+        structure_map = defaultdict(list)
+        equivalence = {}
+        chain_to_mol_id = {}
+        reference_chain_map = {}
+
+        for index, mol in enumerate(system.molecules):
+            chain_id = next(iter(mol.nodes.values()))['chain']
+            resseq = tuple((n['resname'], n['atomname']) for _, n in sorted(mol.nodes.items()))
+            bonds = tuple(sorted((min(a, b), max(a, b)) for a, b in mol.edges))
+            sig = (resseq, bonds)
+            structure_map[sig].append((chain_id, mol))
+
+            chain_to_mol_id[chain_id] = index
+
+        for chain_mol_list in structure_map.values():
+            reference_chain_id = chain_mol_list[0][0]  # chain of the first molecule in the group
+            chains = [chain_id for chain_id, _ in chain_mol_list]
+            for chain_id, mol in chain_mol_list:
+                mol.meta['moltype'] = f"{moltype}_{reference_chain_id}"
+                equivalence[chain_id] = [c for c in chains if c != chain_id]
+                reference_chain_map[chain_id] = reference_chain_id
+
+        system.go_params['reference_chain_map'] = reference_chain_map
+        system.go_params['chain_to_mol_id'] = chain_to_mol_id
+        system.go_params['unique_mols_map'] = equivalence
+
     def run_system(self, system, **kwargs):
         self.kwargs = kwargs
-        self.prepare_run(system, moltype=kwargs['moltype'])
+        if kwargs['go_method'] == 0:
+            self.prepare_run_method_0(system, moltype=kwargs['moltype'])
+        if kwargs['go_method'] == 1:
+            self.prepare_run_method_1(system, moltype=kwargs['moltype'])
         for processor in self.processor_list:
             process_args = inspect.getfullargspec(processor).args
             process_args_values = {arg: self.kwargs[arg] for arg in kwargs.keys() if arg in process_args}
             processor(**process_args_values).run_system(system)
+
         return system
 
 
