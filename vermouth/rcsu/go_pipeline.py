65f8970a4e66b0e46123e16d6218da60f0d09a92
Niek Fraaije <niekfraaije@gmail.com>
2025-05-13 16:46:25 +0200

    Methods are removed, and the seperate molecules are now defined by mol_idx and not by their chain.

--- vermouth/rcsu/go_pipeline.py
+++ vermouth/rcsu/go_pipeline.py
@@ -31,55 +31,37 @@ class GoProcessorPipeline(Processor):
         self.processor_list = processor_list
         self.kwargs = {}
 
-    def prepare_run_method_0(self, system, moltype):
-        """
-        Things to do before running the pipeline.
-        """
-        # merge all molecules in the system
-        # this will eventually become deprecated
-        # with the proper Go-model for multimers
-        vermouth.MergeAllMolecules().run_system(system)
-        molecule = system.molecules[0]
-        molecule.meta['moltype'] = moltype
-
-    def prepare_run_method_1(self, system, moltype):
+    def prepare_run(self, system, moltype):
         structure_map = defaultdict(list)
-        equivalence = {}
-        chain_to_mol_id = {}
-        reference_chain_map = {}
+        unique_mols_map = {}
+        reference_mol_map = {}
 
-        for index, mol in enumerate(system.molecules):
-            chain_id = next(iter(mol.nodes.values()))['chain']
+        for mol in system.molecules:
+            mol_id = next(iter(mol.nodes.values()))['mol_idx']
             resseq = tuple((n['resname'], n['atomname']) for _, n in sorted(mol.nodes.items()))
             bonds = tuple(sorted((min(a, b), max(a, b)) for a, b in mol.edges))
             sig = (resseq, bonds)
-            structure_map[sig].append((chain_id, mol))
+            structure_map[sig].append((mol_id, mol))
 
-            chain_to_mol_id[chain_id] = index
+        for mol_list in structure_map.values():
+            reference_mol_id = mol_list[0][0]  # mol id of the first molecule in the group
+            mol_ids = [mol_id for mol_id, _ in mol_list]
+            for mol_id, mol in mol_list:
+                mol.meta['moltype'] = f"{moltype}_{reference_mol_id}"
+                unique_mols_map[mol_id] = [m_id for m_id in mol_ids if m_id != mol_id]
+                reference_mol_map[mol_id] = reference_mol_id
 
-        for chain_mol_list in structure_map.values():
-            reference_chain_id = chain_mol_list[0][0]  # chain of the first molecule in the group
-            chains = [chain_id for chain_id, _ in chain_mol_list]
-            for chain_id, mol in chain_mol_list:
-                mol.meta['moltype'] = f"{moltype}_{reference_chain_id}"
-                equivalence[chain_id] = [c for c in chains if c != chain_id]
-                reference_chain_map[chain_id] = reference_chain_id
-
-        system.go_params['reference_chain_map'] = reference_chain_map
-        system.go_params['chain_to_mol_id'] = chain_to_mol_id
-        system.go_params['unique_mols_map'] = equivalence
+        system.go_params['reference_mol_map'] = reference_mol_map
+        system.go_params['unique_mols_map'] = unique_mols_map
 
     def run_system(self, system, **kwargs):
         self.kwargs = kwargs
-        if kwargs['go_method'] == 0:
-            self.prepare_run_method_0(system, moltype=kwargs['moltype'])
-        if kwargs['go_method'] == 1:
-            self.prepare_run_method_1(system, moltype=kwargs['moltype'])
+        self.prepare_run(system, moltype=kwargs['moltype'])
+
         for processor in self.processor_list:
             process_args = inspect.getfullargspec(processor).args
             process_args_values = {arg: self.kwargs[arg] for arg in kwargs.keys() if arg in process_args}
             processor(**process_args_values).run_system(system)
-
         return system
 
 
